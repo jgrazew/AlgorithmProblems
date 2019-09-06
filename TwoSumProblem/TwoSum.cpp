ee36ed3812d3659014152582340821d9401419ad
Joe Grazewski <jgrazew@gmail.com>
2019-09-06 16:48:46 -0400

    Update TwoSum.cpp

--- TwoSumProblem/TwoSum.cpp
+++ TwoSumProblem/TwoSum.cpp
@@ -4,6 +4,8 @@ public:
         vector<int> answer; 
         map<int, int> hashMap;
         
+        //we are trying to find the other number that would match with nums[i] to equal the target, i.e we are seeing if that number
+        //is laready in the hashMap. If not we will add another number to search for otherwise we dropdown and return the index's
         for(int i = 0; i != nums.size(); i++){
             if(hashMap.find(target-nums[i]) == hashMap.end()){
                 hashMap.emplace(nums[i], i);
