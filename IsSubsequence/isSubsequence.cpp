a4430a4a681f5250ab303a529c61e1d5845b6d96
Joe Grazewski <jgrazew@gmail.com>
2021-07-05 22:30:44 -0400

    Update isSubsequence.cpp

--- IsSubsequence/isSubsequence.cpp
+++ IsSubsequence/isSubsequence.cpp
@@ -2,6 +2,22 @@
 //then I would loop through the t string and decrement the value. I would expect all numbers to be zero then return true. otherwise return false
 //BUT this doesnt check if they are in the same order so it didnt work. 
 //The approach below makes sure that the order holds up
+
+//Another way we could do this is use a stack. W could loop through each 
+//stack<char> a, b;
+//        for(int i=0;i<s.size();i++) a.push(s[i]);
+//        for(int i=0;i<t.size();i++) b.push(t[i]);
+//        
+//        while(!a.empty() && !b.empty()) {
+//            if(a.top() == b.top()) {
+//                a.pop(), b.pop();
+//            } else {
+//                b.pop();
+//            }
+//        }
+        
+//        if(a.empty()) return true;
+//        return false;
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
